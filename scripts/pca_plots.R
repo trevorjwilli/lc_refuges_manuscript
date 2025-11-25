ab7205db48de0e6d355c3ade8f705ca736941e65
trevorjwilli <trevorjwilli@gmail.com>
2025-11-24 22:11:46 -0500

    Add files for PCA plots

--- scripts/pca_plots.R
+++ scripts/pca_plots.R
@@ -1,141 +1,97 @@
 library(tidyverse)
+library(ggsci)
+library(ggpubr)
 
-setwd('~/dev/lc_radseq_2025/all_analyses_20250225/')
-
-
-# stacks ------------------------------------------------------------------
-
-evec <- read.table('data/stacks/pca/pca.evec')
-colnames(evec) <- c('sample', paste0('PC', 1:20), 'pop')
-
-eigen <- read.table('data/stacks/pca/pca.eval')
-eigen <- eigen |>
-  mutate(percent = V1/sum(V1))
-
-admix <- read.table('data/stacks/admixture/admixture_snps.K7r2.Q')
-fam <- read.table('data/stacks/admixture/admixture_snps.fam')
-
-colnames(admix) <- paste0('K', 1:7)
-admix$sample <- fam$V2
-admix$pop <- fam$V1
-
-admix_long <- admix |>
-  pivot_longer(1:7)
-
-cbPalette <- c("#009E73", "#E69F00", "#56B4E9", "#999999", "#D55E00", "#0072B2","#F0E442")
-names(cbPalette) <- c('K1', 'K2', 'K3', 'K4', 'K5', 'K6', 'K7')
-
-admix_fil <- admix_long |>
-  group_by(sample) |>
-  filter(value == max(value))
-
-evec <- evec |>
-  mutate(sample = gsub('\\w+[:](.*)', '\\1', sample)) |>
-  left_join(admix_fil, by = 'sample') |>
-  mutate(pop = str_replace(pop.x, "_", " "))
+setwd('~/dev/lc_refuges_manuscript/')
 
 
-ggplot(evec, aes(PC1, PC2, shape = pop, fill = name, color = pop)) +
-  geom_point(size = 3) +
-  scale_fill_manual(values = cbPalette, guide = 'none') +
-  scale_color_manual(values = c('black', 'black',  "#56B4E9",'black',"#E69F00", "#999999",  'black', 'black'), guide='none') +
-  scale_shape_manual(values = c(22,23,8,25,10,7,21,24), 
-                     labels = c('Bishop', 'Clear Lake', 'Gandy', 'Leland Harris',
-                                'Mills Valley', 'Mona', 'Gunnison', 'Snake'),
-                     guide = guide_legend(override.aes=list(
-                       shape = c(22,23,8,25,10,7,21,24),
-                       color = c('black', 'black',"#56B4E9",'black',"#E69F00", "#999999",  'black', 'black'),
-                       fill = c("#56B4E9", "#0072B2", "#56B4E9","#F0E442", "#E69F00", "#999999",  "#D55E00", "#009E73")))) +
-  coord_fixed(0.0501529549/0.0739751967) +
-  xlab('PC1 (7.4%)') +
-  ylab('PC2 (5.0%)') +
-  theme_bw() +
-  theme(legend.title = element_blank(),
-        axis.text = element_text(size = 7),
-        axis.title = element_text(size = 7),
-        legend.text = element_text(size = 7))
+#' Plot PCA Data
+#' 
+#' Plots PCA data created by smartpca that is located in the specified directory
+#' 
+#' @param path Path to directory with `pca` subdirectory within it
+#' @param output_name Name to write out plot to
+#' @param return What data to return, if 'data.frame' returns the data frame if 
+#' 'plot' returns the plot.
+#' 
+#' @return Dataframe with formatted pca data and a ggplot graph
+plot_pca <- function(path, outpath=NA, returns='plot') {
+  
+  stopifnot(returns %in% c('plot', 'data.frame'))
+  
+  # Read in eigenvalues
+  eigen_path <- file.path(path, 'pca', 'pca.eval')
+  eigen <- read.table(eigen_path)
+  eigen <- eigen |>
+    mutate(percent = V1/sum(V1))
+  percent_pc1 <- eigen$percent[1]
+  percent_pc2 <- eigen$percent[2]
+  
+  # Make list of source poopulation names
+  source_pops <- c('Bishop', 'Clear Lake', 'Gandy', 'Leland Harris',
+                   'Mills Valley', 'Mona')
+  
+  # Read in PC scores
+  evec_path <- file.path(path, 'pca', 'pca.evec')
+  evec <- read.table(evec_path)
+  colnames(evec) <- c('sample', paste0('PC', 1:20), 'pop')
+  
+  # Wrangle data
+  evec <- evec |>
+    mutate(
+      sample = gsub('\\w+[:](.*)', '\\1', sample),
+      pop = str_replace(pop, "_", " "),
+      refuge = case_when(
+        pop %in% source_pops ~ 'Source',
+        .default = 'Refuge'
+      )
+    )
+  
+  # Plot PCA
+  p <- ggplot(evec, aes(PC1, PC2, shape = pop, color = refuge)) +
+    geom_point(size = 2) +
+    scale_color_d3() +
+    coord_fixed(percent_pc2/percent_pc1) +
+    xlab(paste0('PC1 (', round(percent_pc1*100, digits=1), '%)')) +
+    ylab(paste0('PC2 (', round(percent_pc2*100, digits=1), '%)')) +
+    theme_bw() +
+    guides(color = "none", shape = guide_legend(title=NULL)) +
+    theme(legend.title = element_blank(),
+          axis.text = element_text(size = 7),
+          axis.title = element_text(size = 7),
+          legend.text = element_text(size = 5))
+  
+  # If output path given, save plot
+  if(!is.na(outpath)) {
+    print(paste0('Writing plot to ', path))
+    
+    ggsave(paste0(outpath, '.png'), plot=p, device = 'png',
+           width = 140, height = 100, units = 'mm', dpi = 300)
+    
+    ggsave(paste0(outpath, '.pdf'), plot=p, device = 'pdf',
+           width = 140, height = 100, units = 'mm', dpi = 300)
+  }
 
-ggsave('figures/stacks_pca.png', device = 'png',
-       width = 140, height = 100, units = 'mm', dpi = 300)
+  print(p)
+  if(returns == 'plot') {
+    return(p)
+  } else {
+    return(evec)
+  }
+}
 
-ggsave('figures/stacks_pca.pdf', device = 'pdf',
-       width = 140, height = 100, units = 'mm', dpi = 300)
+b_plot <- plot_pca('data/bishop/', outpath = 'data/bishop/pca_plot')
+cl_plot <- plot_pca('data/clear_lake/', outpath = 'data/clear_lake/pca_plot')
+g_plot <- plot_pca('data/gandy/', outpath = 'data/gandy/pca_plot')
+lh_plot <- plot_pca('data/leland_harris/', outpath = 'data/leland_harris/pca_plot')
+mv_plot <- plot_pca('data/mills_valley/', outpath = 'data/mills_valley/pca_plot')
+mo_plot <- plot_pca('data/mona/', outpath = 'data/mona/pca_plot')
 
+ggarrange(b_plot, lh_plot, g_plot, mv_plot, cl_plot, mo_plot, 
+          ncol=2, nrow=3, labels=LETTERS,
+          font.label=list(size=8, color='black', face='bold'))
 
-# ipyrad ------------------------------------------------------------------
-
-
-evec <- read.table('data/ipyrad/pca/pca.evec')
-colnames(evec) <- c('sample', paste0('PC', 1:20), 'pop')
-
-eigen <- read.table('data/ipyrad/pca/pca.eval')
-eigen <- eigen |>
-  mutate(percent = V1/sum(V1))
-
-admix <- read.table('data/ipyrad/admixture/plink.K7r5.Q')
-fam <- read.table('data/ipyrad/plink.fam')
-
-fam <- fam |>
-  mutate(pop = gsub('[0-9]+_(.+)_LTC.+', '\\1', V2),
-    pop = case_when(
-    pop %in% c('Bishop', 'Red_Knolls') ~ 'Bishop',
-    pop %in% c('Clear_Lake', 'Willow_Pond') ~ 'Clear Lake',
-    pop %in% c('Gandy', 'Keg_Springs') ~ 'Gandy',
-    pop %in% c('Mona', 'Big_Springs', 'Jail_Pond', 'Tooele_Army') ~ 'Mona',
-    pop %in% c('Leland_Harris', 'Lower_Rocky') ~ 'Leland Harris',
-    pop %in% c('Cluster', 'Copilot', 'Mills_Valley', 'Rosebud_Top_P') ~ 'Mills Valley',
-    pop %in% c('Cartier_Slough', 'Deer_Parks', 'Harris_Ponds', 'Henrys_Fork') ~ 'Snake',
-    pop %in% c('Mud_Basin') ~ 'Mud Basin'
-  ))
-
-colnames(admix) <- paste0('K', 1:7)
-admix$sample <- fam$V2
-admix$pop <- fam$pop
-
-admix_long <- admix |>
-  pivot_longer(1:7)
-
-admix_fil <- admix_long |>
-  group_by(sample) |>
-  filter(value == max(value))
-
-evec <- evec |>
-  mutate(sample = gsub('\\w+[:](.*)', '\\1', sample)) |>
-  left_join(admix_fil, by = 'sample')
-
-evec_mean <- evec |>
-  group_by(pop.y) |>
-  summarise(PC1 = mean(PC1), PC2 = mean(PC2))
-
-ggplot() +
-  geom_point(data = evec, aes(PC1, PC2, color = name)) +
-  geom_text(data = evec_mean, aes(x=PC1, y=PC2, label=pop.y))
-
-cbPalette <- c("#D55E00",  "#0072B2", "#009E73", "#F0E442", "#999999",  "#E69F00", "#56B4E9")
-names(cbPalette) <- c('K1', 'K2', 'K3', 'K4', 'K5', 'K6', 'K7')
-
-ggplot(evec, aes(PC1, PC2, shape = pop.y, fill = name, color = pop.y)) +
-  geom_point(size = 3) +
-  scale_fill_manual(values = cbPalette, guide = 'none') +
-  scale_color_manual(values = c('black', 'black',  "#D55E00",'black',"#E69F00", "#999999",  'black', 'black'), guide='none') +
-  scale_shape_manual(values = c(22,23,8,25,10,7,21,24), 
-                     labels = c('Bishop', 'Clear Lake', 'Gandy', 'Leland Harris',
-                                'Mills Valley', 'Mona', 'Gunnison', 'Snake'),
-                     guide = guide_legend(override.aes=list(
-                       shape = c(22,23,8,25,10,7,21,24),
-                       color = c('black', 'black',"#D55E00",'black',"#E69F00", "#999999",  'black', 'black'),
-                       fill = c("#56B4E9", "#0072B2", "#56B4E9","#F0E442", "#E69F00", "#999999",    "#E69F00", "#009E73")))) +
-  coord_fixed(eigen$percent[2]/eigen$percent[1]) +
-  xlab('PC1 (18.5%)') +
-  ylab('PC2 (15.4%)') +
-  theme_bw() +
-  theme(legend.title = element_blank(),
-        axis.text = element_text(size = 7),
-        axis.title = element_text(size = 7),
-        legend.text = element_text(size = 7))
-
-ggsave('figures/ipyrad_pca.png', device = 'png',
-       width = 140, height = 112, units = 'mm', dpi = 300)
-
-ggsave('figures/ipyrad_pca.pdf', device = 'pdf',
-       width = 140, height = 112, units = 'mm', dpi = 300)
+ggsave('figures/all_pca_plots.pdf', device='pdf', width=168, height=168,
+       units='mm', dpi=300)
+ggsave('figures/all_pca_plots.png', device='png', width=168, height=168,
+       units='mm', dpi=300)
